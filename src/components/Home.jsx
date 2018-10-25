fb512628dd472bfdcbcb21d9dfd86985d9413759
Jennifer Kennedy <jenniferkennedy@Jennifers-MacBook-Pro.local>
2018-10-25 12:14:28 -0700

    general styles and favicon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './styles.css';
 
 export class Home extends React.Component {
    render() {
       return (
-         <div>
+         <div className="homeContainer">
             <h1>Jennifer Williams</h1>
          </div>
       );
