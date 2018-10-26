ebbab25a407a7e03e163f417ce43953fd87b520b
Jennifer Williams <jenken131@gmail.com>
2018-10-25 20:09:56 -0700

    stylized about icons (#4)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './styles.css';
 
 export class About extends React.Component {
    render() {
       return (
-         <div>
+         <div className="aboutContainer">
             <h1>About Me</h1>
             <p>
                My name is Jennifer Williams and I am a front-end developer in
@@ -14,13 +15,37 @@ export class About extends React.Component {
                esports, cuddling my dog, and rock climbing!
             </p>
             <h2>My Skills</h2>
-            <div>
-               <FontAwesomeIcon icon={['fab', 'react']} />
-               <FontAwesomeIcon icon={['fab', 'html5']} />
-               <FontAwesomeIcon icon={['fab', 'css3-alt']} />
-               <FontAwesomeIcon icon={['fab', 'js-square']} />
-               <FontAwesomeIcon icon={['fab', 'git-square']} />
-               <FontAwesomeIcon icon={['fab', 'github']} />
+            <div className="skillsContainer">
+               <FontAwesomeIcon
+                  style={{ color: '#61dafb', margin: '5px' }}
+                  size="5x"
+                  icon={['fab', 'react']}
+               />
+               <FontAwesomeIcon
+                  style={{ color: '#E44D26', margin: '5px' }}
+                  size="5x"
+                  icon={['fab', 'html5']}
+               />
+               <FontAwesomeIcon
+                  style={{ color: '#1572B6', margin: '5px' }}
+                  size="5x"
+                  icon={['fab', 'css3-alt']}
+               />
+               <FontAwesomeIcon
+                  style={{ color: '#F0DB4F', margin: '5px' }}
+                  size="5x"
+                  icon={['fab', 'js-square']}
+               />
+               <FontAwesomeIcon
+                  style={{ color: '#f14e32', margin: '5px' }}
+                  size="5x"
+                  icon={['fab', 'git-square']}
+               />
+               <FontAwesomeIcon
+                  style={{ color: '#24292e', margin: '5px' }}
+                  size="5x"
+                  icon={['fab', 'github']}
+               />
             </div>
          </div>
       );
