e77e26b7673df60e5ba8650d5e36d46c5e4ce45b
Jennifer Williams <jenken131@gmail.com>
2018-10-25 20:29:32 -0700

    added gutter styles (#5)

--- src/App.js
+++ src/App.js
@@ -10,17 +10,19 @@ import { Contact } from './components/Contact';
 class App extends Component {
    render() {
       return (
-         <Router>
-            <>
-               <NavBar />
-               <Switch>
-                  <Route path="/contact" component={Contact} />
-                  <Route path="/about" component={About} />
-                  <Route path="/projects" component={Projects} />
-                  <Route path="/" component={Home} />
-               </Switch>
-            </>
-         </Router>
+         <div className="pageContainer">
+            <Router>
+               <>
+                  <NavBar />
+                  <Switch>
+                     <Route path="/contact" component={Contact} />
+                     <Route path="/about" component={About} />
+                     <Route path="/projects" component={Projects} />
+                     <Route path="/" component={Home} />
+                  </Switch>
+               </>
+            </Router>
+         </div>
       );
    }
 }
