1f5d34fad1813b046c9c06044a5d158ab0e4d21d
Jennifer Williams <jenken131@gmail.com>
2018-10-29 21:27:19 -0700

    changed contact page styles and home page layout/styles (#9)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './styles.css';
 
+const ICON_SIZE = '6x';
+
 export class Contact extends React.Component {
    render() {
       return (
@@ -12,38 +14,58 @@ export class Contact extends React.Component {
          <div className="aboutContainer">
             <h1>Contact Me!</h1>
             <div className="contactContainer">
-               <a href="https://twitter.com/TaIiaa_">
+               <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://twitter.com/TaIiaa_"
+               >
                   <FontAwesomeIcon
-                     style={{ color: '#1da1f2', margin: '5px' }}
-                     size="2x"
+                     style={{ color: '#1da1f2' }}
+                     size={ICON_SIZE}
                      icon={['fab', 'twitter']}
                   />
                </a>
-               <a href="https://www.linkedin.com/in/jennifer-williams-1b0a54105/">
+               <a
+                  href="https://www.linkedin.com/in/jennifer-williams-1b0a54105/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+               >
                   <FontAwesomeIcon
-                     style={{ color: '#0073b1', margin: '5px' }}
-                     size="2x"
+                     style={{ color: '#0073b1' }}
+                     size={ICON_SIZE}
                      icon={['fab', 'linkedin']}
                   />
                </a>
-               <a href="https://codepen.io/Taliaa/">
+               <a
+                  href="https://codepen.io/Taliaa/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+               >
                   <FontAwesomeIcon
-                     style={{ color: 'black', margin: '5px' }}
-                     size="2x"
+                     style={{ color: 'black' }}
+                     size={ICON_SIZE}
                      icon={['fab', 'codepen']}
                   />
                </a>
-               <a href="https://www.freecodecamp.org/taljjaa">
+               <a
+                  href="https://www.freecodecamp.org/taljjaa"
+                  target="_blank"
+                  rel="noopener noreferrer"
+               >
                   <FontAwesomeIcon
-                     style={{ color: 'darkgreen', margin: '5px' }}
-                     size="2x"
+                     style={{ color: 'darkgreen' }}
+                     size={ICON_SIZE}
                      icon={['fab', 'free-code-camp']}
                   />
                </a>
-               <a href="https://github.com/Taljjaa">
+               <a
+                  href="https://github.com/Taljjaa"
+                  target="_blank"
+                  rel="noopener noreferrer"
+               >
                   <FontAwesomeIcon
-                     style={{ color: '#24292e', margin: '5px' }}
-                     size="2x"
+                     style={{ color: '#24292e' }}
+                     size={ICON_SIZE}
                      icon={['fab', 'github']}
                   />
                </a>
