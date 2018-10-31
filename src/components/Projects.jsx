9d718a42453478f2e3fbe9825773d40fcb5588b3
Jennifer Williams <jenken131@gmail.com>
2018-10-30 21:35:19 -0700

    huge commit idk what i did (#10)

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
-import randomQuoteImg from '../images/randomQuote.png';
-import JavaScriptCalc from '../images/JavaScriptCalculator.png';
+import randomQuoteImg from '../images/RandomQuote2.png';
+import JavaScriptCalc from '../images/JavaScriptCalc.png';
 import GuessGaming from '../images/GuessingGame.png';
+import Pomodoro from '../images/PomodoroClock.png';
+import TwitchLayout from '../images/TwitchAPI.png';
+import TicTac from '../images/TicTac.png';
 import './styles.css';
 
 export class Projects extends React.Component {
    render() {
       return (
          <div>
-            <h1>Projects</h1>
-            <div className="flexContainer">
-               <img className="item" src={randomQuoteImg} alt="" />
-               <img className="item" src={JavaScriptCalc} alt="" />
-               <img className="item" src={GuessGaming} alt="" />
+            <div className="basicFlexContainer">
+               <h1>Projects</h1>
+
+               <div className="projectsContainer">
+                  <img src={randomQuoteImg} alt="" />
+                  <img src={JavaScriptCalc} alt="" />
+                  <img src={GuessGaming} alt="" />
+                  <img src={Pomodoro} alt="" />
+                  <img src={TwitchLayout} alt="" />
+                  <img src={TicTac} alt="" />
+               </div>
+               <p>
+                  To view my other projects visit my&nbsp;
+                  <a
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     href="https://github.com/Taljjaa"
+                  >
+                     Github
+                  </a>
+               </p>
             </div>
-            <p>
-               To view my other projects visit my&nbsp;
-               <a href="https://github.com/Taljjaa">Github</a>
-            </p>
          </div>
       );
    }
